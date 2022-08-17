56ece53a02145a0e5be5389a1394b9497c9cc0cf
sthakrishnakumar <sthakrishnakumar@gmail.com>
2022-08-17 13:10:37 +0545

    quiz app apiclient created

---
+++ lib/core/api_client.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:quiz_app/core/app_constant.dart';
+
+class ApiClient {
+  Future request({
+    required endpoint,
+    String type = 'get',
+    Map<String, dynamic> data = const {},
+  }) async {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: AppConstant.baseUrl,
+        headers: {
+          'Content-Type': 'application/json',
+          "Accept": 'application/json',
+        },
+      ),
+    );
+
+    try {
+      final result = type == 'get'
+          ? await dio.get(endpoint)
+          : await dio.post(
+              endpoint,
+              data: data,
+            );
+
+      return result.data;
+    } on DioError catch (e) {
+      throw Exception(e.message);
+    }
+  }
+}
---
+++ lib/core/app_constant.dart
@@ -0,0 +1,5 @@
+class AppConstant {
+  AppConstant._();
+  static const String baseUrl = 'https://the-trivia-api.com/api/questions';
+  static const String quiz = 'questions';
+}
